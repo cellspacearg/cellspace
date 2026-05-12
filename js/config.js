abcdf04d50a2ea260b99ad564394fd697ebdc33a
cellspacearg <cellspacearg@gmail.com>
2026-05-12 14:41:09 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+// Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCNHshneVJhwu6XvxrYAT52w4O8UM_9P94",
   authDomain: "cellspace-login.firebaseapp.com",
@@ -7,8 +8,13 @@ const firebaseConfig = {
   appId: "1:312985834328:web:4a99d20f26427d48300b2d"
 };
 
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Referencias a servicios de Firebase
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+// 👑 EMAIL DE ADMINISTRADOR (SOLO VOS PODÉS ENTRAR AL ADMIN)
 const ADMIN_EMAIL = "nahuel0123encinas@gmail.com";
